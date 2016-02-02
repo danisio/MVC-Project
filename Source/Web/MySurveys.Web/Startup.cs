c4529de090e2580ed188a9617cde0906c1274486
danisio <dany_s@abv.bg>
2016-02-02 16:44:33 +0200

    Initial commit, updated packages

---
+++ Source/Web/MySurveys.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MySurveys.Web.Startup))]
+namespace MySurveys.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
