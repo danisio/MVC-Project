1ccabd4583d2385fcf6c93f3f9a60c4424dbaa4e
danisio <dany_s@abv.bg>
2016-02-05 00:43:11 +0200

    appharbor problem again

--- Source/Web/MySurveys.Web/Global.asax.cs
+++ Source/Web/MySurveys.Web/Global.asax.cs
@@ -1,12 +1,9 @@
 ﻿namespace MySurveys.Web
 {
-    using System.Data.Entity;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
-    using Data;
-    using Data.Migrations;
     using Infrastructure.Mapping;
 
     public class MvcApplication : HttpApplication
@@ -20,8 +17,6 @@
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MySurveysDbContext, Configuration>());
         }
     }
 }
