dcbdfea54d717c2ed3649f5a22f7f0ed889b84e1
danisio <dany_s@abv.bg>
2016-02-05 10:28:36 +0200

    appharbor

--- Source/Data/MySurveys.Data/MySurveysDbContext.cs
+++ Source/Data/MySurveys.Data/MySurveysDbContext.cs
@@ -15,13 +15,14 @@
         {
         }
 
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    Database.SetInitializer(new MigrateDatabaseToLatestVersion<MySurveysDbContext, Configuration>());
-        //    modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
-        //    modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
-        //    modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
-        //}
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MySurveysDbContext, Configuration>());
+            //modelBuilder.Entity<IdentityUserLogin>().HasKey<string>(l => l.UserId);
+            //modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id);
+            //modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
+        }
 
         public virtual IDbSet<Question> Questions { get; set; }
 
--- Source/Web/MySurveys.Web/Global.asax.cs
+++ Source/Web/MySurveys.Web/Global.asax.cs
@@ -1,13 +1,10 @@
 ﻿namespace MySurveys.Web
 {
-    using System.Data.Entity;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
     using Infrastructure.Mapping;
-    using Data;
-    using Data.Migrations;
 
     public class MvcApplication : HttpApplication
     {
@@ -20,8 +17,6 @@
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MySurveysDbContext, Configuration>());
         }
     }
 }
