72c72c75142a3a67a04dd62b5afb9a32d071e347
danisio <dany_s@abv.bg>
2016-02-21 23:38:02 +0200

    added possible answers to Administration/Controllers/PossibleAnswersController.cs

---
+++ Source/Data/MySurveys.Services/PossibleAnswerService.cs
@@ -0,0 +1,49 @@
+﻿namespace MySurveys.Services
+{
+    using System.Linq;
+    using Data.Repository;
+    using Models;
+
+    public class PossibleAnswerService
+    {
+        private IRepository<PossibleAnswer> possibleAnswers;
+
+        public PossibleAnswerService(IRepository<PossibleAnswer> possibleAnswers)
+        {
+            this.possibleAnswers = possibleAnswers;
+        }
+
+        public IQueryable<PossibleAnswer> GetAll()
+        {
+            return this.possibleAnswers.All();
+        }
+
+        public PossibleAnswer GetById(object id)
+        {
+            return this.possibleAnswers.GetById(id);
+        }
+
+        public PossibleAnswer Add(PossibleAnswer answer)
+        {
+            this.possibleAnswers.Add(answer);
+            this.possibleAnswers.SaveChanges();
+
+            return answer;
+        }
+
+        public PossibleAnswer Update(PossibleAnswer answer)
+        {
+            this.possibleAnswers.Update(answer);
+            this.possibleAnswers.SaveChanges();
+
+            return answer;
+        }
+
+        public void Delete(object id)
+        {
+            this.possibleAnswers.Delete(id);
+            this.possibleAnswers.SaveChanges();
+        }
+
+    }
+}
---
+++ Source/Web/MySurveys.Web/Areas/Administration/ViewModels/PossibleAnswerViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace MySurveys.Web.Areas.Administration.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+    using AutoMapper;
+    using Base;
+    using Models;
+    using MvcTemplate.Web.Infrastructure.Mapping;
+
+    public class PossibleAnswerViewModel : AdministrationViewModel, IMapFrom<PossibleAnswer>, IHaveCustomMappings
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200), MinLength(2)]
+        [UIHint("CustomString")]
+        public string Content { get; set; }
+
+        [Display(Name = "Question Content")]
+        [UIHint("CustomString")]
+        public string QuestionContent { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<PossibleAnswer, PossibleAnswerViewModel>()
+                 .ForMember(r => r.QuestionContent, opt => opt.MapFrom(r => r.Question.Content))
+                 .ReverseMap();
+        }
+    }
+}
