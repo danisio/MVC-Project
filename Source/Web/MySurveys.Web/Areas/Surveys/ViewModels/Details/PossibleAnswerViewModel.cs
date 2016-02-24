f968d4ef272283653080b93755f45bfea4c72db3
danisio <dany_s@abv.bg>
2016-02-24 11:19:27 +0200

    Added Survey Statistics

---
+++ Source/Web/MySurveys.Web/Areas/Surveys/ViewModels/Details/PossibleAnswerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MySurveys.Web.Areas.Surveys.ViewModels.Details
+{
+    using Models;
+    using MvcTemplate.Web.Infrastructure.Mapping;
+
+    public class PossibleAnswerViewModel : IMapFrom<PossibleAnswer>
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
